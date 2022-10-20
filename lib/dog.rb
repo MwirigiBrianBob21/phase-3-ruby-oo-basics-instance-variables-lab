2764134623d7d4c15ff9072234058052a9f3aca6
MwirigiBrianBob21 <brian.mwirigi@students.moringaschool.com>
2022-10-20 23:14:56 +0300

    Complete assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
-class Dog
 
+
+class Dog
+    #setter method
+    def name=(dog_name)
+       @this_dogs_name = dog_name
+    end
+    #getter method
+    def name
+        #access the data from the @this_dogs_name instance variable and return it
+        @this_dogs_name
+    end
 end
+
+ lassie = Dog.new
+ lassie.name = "Lassie"
+
+#  call the Dog#name method
+ puts lassie.name
